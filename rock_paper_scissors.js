5ee63f37d0e61eafba5558e15d8d1d53b04da6ad
demianprogramming <103469634+demianprogramming@users.noreply.github.com>
2025-06-08 22:57:08 -0400

    Generate a random number from 1 to 100

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,2 +1,10 @@
 // Test Hello World Console Linked
 console.log("Hello World");
+
+// Write a funtion that randomly returns "rock", "paper", "scissors".
+// Generate a random number using Mathr.random
+// Use IF ELSE statments to return desired values when a number is generated.
+function getComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * 100 + 1); 
+    return randomNumber;
+}
